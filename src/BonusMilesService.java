171cc545412af901226d7668f19991565f9210cc
Dina18013 <99754434+Dina18013@users.noreply.github.com>
2022-03-28 20:48:44 +0300

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
     public int calculate(int cost) { //функция для расчета миль
         
-        int price = 1000;
+        int price = 10_000;
         int miles = price / 20; //расчет количества миль
         return miles;
     }
